632cc8495a1e8caa1a811d8e88df3e0239937bb2
Maartz <maartz@protonmail.com>
2019-10-19 12:46:03 +0200

    feat: add a random word generator based on dictionnary

--- lib/dictionnary.ex
+++ lib/dictionnary.ex
@@ -1,5 +1,10 @@
 defmodule Dictionnary do
- def hello do
-   IO.puts "Hello, world"
+ def word_list do
+   contents = File.read!("assets/words.txt")
+   list     = String.split(contents, ~r/\n/, trim: true)
+ end
+
+  def random_words() do
+    Enum.random(word_list())
   end
 end
